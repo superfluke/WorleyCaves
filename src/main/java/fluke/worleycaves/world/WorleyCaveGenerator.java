9a6f547f3e85acb66d8aac6331b3967a6dee85f3
Unknown <anon@mail.com>
2018-06-19 19:57:13 -0400

    Add in some perlin to the mix

--- src/main/java/fluke/worleycaves/world/WorleyCaveGenerator.java
+++ src/main/java/fluke/worleycaves/world/WorleyCaveGenerator.java
@@ -9,22 +9,28 @@ import net.minecraft.world.gen.MapGenBase;
 public class WorleyCaveGenerator extends MapGenBase
 {
 	public FastNoise cellnoise = new FastNoise();
+	public FastNoise perlin = new FastNoise();
 	
 	public WorleyCaveGenerator()
 	{
 		cellnoise.SetNoiseType(FastNoise.NoiseType.Cellular);
 		cellnoise.SetCellularReturnType(FastNoise.CellularReturnType.Distance2Div);
+		perlin.SetNoiseType(FastNoise.NoiseType.Perlin);
+		//perlin.SetNoiseType(FastNoise.CellularDistanceFunction.)
 	}
 	
 	protected void recursiveGenerate(World worldIn, int chunkX, int chunkZ, int originalX, int originalZ, ChunkPrimer chunkPrimerIn)
     {
 		if (chunkX == originalX && chunkZ == originalZ)
 		{
-
+			
 			int startHeight = 1;
 			int maxHeight = 90;
-			float amp = (float) 1.5;
-			double cutoff = -0.25;
+			float worleyscale = 1.0F;
+			float worleyamp = 0.9F;
+			int perlinscale = 20;
+			float perlinamp = 0.6F;
+			double cutoff = -0.35;
 			for (int x=0; x<16; x++)
 			{
 				int realX = x + chunkX*16;
@@ -33,8 +39,10 @@ public class WorleyCaveGenerator extends MapGenBase
 					int realZ = z + chunkZ*16;
 					for(int y=startHeight; y<maxHeight; y++)
 						{
-							//System.out.println(cellnoise.GetNoise((float) realX,  (float) y, (float) realZ));
-							if (cellnoise.GetNoise((float) realX*amp,  (float) y*amp, (float) realZ*amp) > cutoff)
+							float cellvalue = cellnoise.GetNoise((float) realX*worleyscale,  (float) y*worleyscale, (float) realZ*worleyscale) * worleyamp;
+							float perlinvalue = ((perlin.GetNoise((float) realX*perlinscale,  (float) y*perlinscale, (float) realZ*perlinscale)+1)/2) * perlinamp;
+							float noise = cellvalue - perlinvalue;
+							if ( noise > cutoff)
 							{
 								chunkPrimerIn.setBlockState(x, y, z, Blocks.AIR.getDefaultState());
 							}
