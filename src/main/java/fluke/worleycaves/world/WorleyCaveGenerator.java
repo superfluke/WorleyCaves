2272fbfa7df72f6d27ca26373f637b8bdbc80d69
Unknown <anon@mail.com>
2018-06-25 23:37:47 -0400

    Merge booleanbyte upgrades and play with cutoff adjuster

--- src/main/java/fluke/worleycaves/world/WorleyCaveGenerator.java
+++ src/main/java/fluke/worleycaves/world/WorleyCaveGenerator.java
@@ -10,6 +10,7 @@ public class WorleyCaveGenerator extends MapGenBase
 {
 	private FastNoise worleyF1divF3 = new FastNoise();
 	private FastNoise perlin = new FastNoise();
+	private FastNoise simplex = new FastNoise();
 	
 	private int startHeight = 1;
 	private int maxHeight = 90;
@@ -18,26 +19,35 @@ public class WorleyCaveGenerator extends MapGenBase
 	public WorleyCaveGenerator()
 	{
 		worleyF1divF3.SetNoiseType(FastNoise.NoiseType.Cellular);
-		worleyF1divF3.SetFrequency(0.02f);
+		worleyF1divF3.SetFrequency(0.016f);
 		worleyF1divF3.SetCellularReturnType(FastNoise.CellularReturnType.Distance1Div3);
 		worleyF1divF3.SetCellularJitter(0.3f);
 		
 		perlin.SetNoiseType(FastNoise.NoiseType.Perlin);
 		perlin.SetFrequency(0.05f);
+		
+		simplex.SetNoiseType(FastNoise.NoiseType.Simplex);
+		simplex.SetFrequency(0.05f);
+		
 	}
 	
 	protected void recursiveGenerate(World worldIn, int chunkX, int chunkZ, int originalX, int originalZ, ChunkPrimer chunkPrimerIn)
     {
-		//Values accessible for hot-swapping in debug mode
-		//TODO move good values to constructor
-		cutoff = -0.92f;
-		worleyF1divF3.SetCellularJitter(1.0f);
-		worleyF1divF3.SetCellularReturnType(FastNoise.CellularReturnType.Distance3Sub1);
-		worleyF1divF3.SetCellularDistanceFunction(FastNoise.CellularDistanceFunction.Euclidean);
-		long millis = System.currentTimeMillis();
+		
+		
+		
 		
 		if (chunkX == originalX && chunkZ == originalZ)
 		{
+			//Values accessible for hot-swapping in debug mode
+			//TODO move good values to constructor
+			cutoff = -0.20f;
+			worleyF1divF3.SetCellularJitter(1.0f);
+			worleyF1divF3.SetCellularReturnType(FastNoise.CellularReturnType.Distance1Div3);
+			worleyF1divF3.SetCellularDistanceFunction(FastNoise.CellularDistanceFunction.Euclidean);
+//			worleyF1divF3.SetFrequency(0.016f);
+			long millis = System.currentTimeMillis();
+			
 			for (int x=0; x<16; x++)
 			{
 				int realX = x + chunkX*16;
@@ -46,14 +56,22 @@ public class WorleyCaveGenerator extends MapGenBase
 					int realZ = z + chunkZ*16;
 					for(int y=startHeight; y<maxHeight; y++)
 					{
-						float dispAmp = 15.0f;
+						float funAdjuster = (2 * simplex.GetNoise(realX-2, y, realZ/2))/10;
+//						System.out.println(funAdjuster);
+//						worleyF1divF3.SetFrequency(0.016f + funAdjuster);
+						cutoff = -0.18f + funAdjuster;
+						float dispAmp = 7.0f;
 						
 						//Experiment making the cave system more chaotic the more you descend 
-//						float dispAmp = 0.3f * (maxHeight-y);
+//						dispAmp = 0.3f * (maxHeight-y);
 						
-						float xDisp = perlin.GetNoise(realX, y, realZ)*dispAmp;
-						float yDisp = perlin.GetNoise(realX, y-256.0f, realZ)*dispAmp;
-						float zDisp = perlin.GetNoise(realX, y-512.0f, realZ)*dispAmp;
+						float xDisp = 0f;
+						float yDisp = 0f;
+						float zDisp = 0f;
+						
+//						xDisp = perlin.GetNoise(realX, y, realZ)*dispAmp;
+//						yDisp = perlin.GetNoise(realX, y-256.0f, realZ)*dispAmp;
+//						zDisp = perlin.GetNoise(realX, y-512.0f, realZ)*dispAmp;
 						
 						//Different experiment for displacement
 //						float dispAmpNoise = perlin.GetNoise(realX, y, realZ) * dispAmp;
@@ -65,7 +83,7 @@ public class WorleyCaveGenerator extends MapGenBase
 						//Multiplying doubling the y frequency to get some more caves
 						float noise = worleyF1divF3.SingleCellular3Edge(realX+xDisp, y*2.0f+yDisp, realZ+zDisp);
 						
-						if (noise < cutoff)
+						if (noise > cutoff)
 						{
 							//Diggy diggy hole
 							chunkPrimerIn.setBlockState(x, y, z, Blocks.AIR.getDefaultState());
@@ -77,9 +95,10 @@ public class WorleyCaveGenerator extends MapGenBase
 					}
 				}
 			}
+			System.out.println("Current cave gen time [ms]: " + (System.currentTimeMillis() - millis));
 		}
 		
-		System.out.println("Current cave gen time [ms]: " + (System.currentTimeMillis() - millis));
+		
     }
 
 }
