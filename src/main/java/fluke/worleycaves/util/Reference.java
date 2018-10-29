1b147fa9f53525a600644d3bef8559b3cc45456a
Unknown <anon@mail.com>
2018-10-29 10:15:19 -0400

    Version # update for 1.10.2

--- src/main/java/fluke/worleycaves/util/Reference.java
+++ src/main/java/fluke/worleycaves/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "worleycaves";
 	public static final String NAME = "Worley Caves";
-	public static final String VERSION = "1.3.0";
+	public static final String VERSION = "1.10.2-1.3.0";
 	public static final String ACCEPTED_VERSIONS = "[1.10.2]";
 	public static final String CLIENT_PROXY_CLASS = "fluke.worleycaves.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "fluke.worleycaves.proxy.CommonProxy";
